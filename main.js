7cf33ee57208c35ed4340410380e5b2e8f5f7ff4
tran quang lam <tranquanglamtp@gmail.com>
2021-03-12 01:34:09 +0900

    tạo js

---
+++ main.js
@@ -0,0 +1,28 @@
+var input=document.querySelector('.input');
+var items=Array.from(document.querySelectorAll('.item'));
+items.forEach(function(bnt){
+    bnt.addEventListener('click',function(){
+        if(input.innerHTML=='0')
+        input.innerHTML='0';
+        if(bnt.innerHTML=='AC')
+        input.innerHTML='';
+        else
+            if(bnt.innerHTML=='DEL'){
+                var arrtext=Array.from(input.innerHTML);
+                arrtext.splice(arrtext.length-1,1);
+                if(arrtext.length!=0)
+                input.innerHTML=arrtext.join('');
+                else input.innerHTML= '0';
+            }
+        else
+            if(bnt.innerHTML=='%'){
+                input.innerHTML=input.innerHTML/100
+            }   
+        else
+            if(bnt.innerHTML=='='){
+                input.innerHTML=eval(input.innerHTML);
+            }
+        else
+            input.innerHTML+=bnt.innerHTML;
+    })
+})
